3010f3ad718294c6bd41dfdfd2ad16ce6a7cc588
Nagy Adam <raween2000@gmail.com>
2023-01-07 21:06:26 +0100

    JwtService

--- src/main/java/com/adamnagyan/yahoofinancewebapi/configurations/JwtAuthenticationFilter.java
+++ src/main/java/com/adamnagyan/yahoofinancewebapi/configurations/JwtAuthenticationFilter.java
@@ -2,6 +2,9 @@ package com.adamnagyan.yahoofinancewebapi.configurations;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
@@ -28,6 +32,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
     ;
     jwtToken = authHeader.substring(7);
-    userEmail = jwtService.extractUsername(jwtToken); //todo extract from JWT
+    userEmail = jwtService.extractUsername(jwtToken);
+    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+    }
   }
 }
