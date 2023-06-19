4c7ff59f98bd93595ad6c97958ed348698e9fb7a
Nagy Adam <raween2000@gmail.com>
2023-06-19 22:01:47 +0200

    Refactoring

--- src/main/java/com/adamnagyan/yahoofinancewebapi/stock/repository/StockRepositoryImpl.java
+++ src/main/java/com/adamnagyan/yahoofinancewebapi/stock/repository/StockRepositoryImpl.java
@@ -21,190 +21,189 @@ import java.util.*;
 @RequiredArgsConstructor
 public class StockRepositoryImpl implements StockRepository {
 
-	private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
 
-	private final ObjectMapper objectMapper;
+  private final ObjectMapper objectMapper;
 
-	@Value("${external-apis.yahoofinance-cashflow}")
-	private String cashFlowStatementApi;
+  @Value("${external-apis.yahoofinance-cashflow}")
+  private String cashFlowStatementApi;
 
-	@Value("${external-apis.yahoofinance-cashflow-quarterly}")
-	private String cashFlowStatementQuarterlyApi;
+  @Value("${external-apis.yahoofinance-cashflow-quarterly}")
+  private String cashFlowStatementQuarterlyApi;
 
-	@Value("${external-apis.yahoofinance-income-statements}")
-	private String incomeStatementsApi;
+  @Value("${external-apis.yahoofinance-income-statements}")
+  private String incomeStatementsApi;
 
-	@Value("${external-apis.yahoofinance-income-statements-quarterly}")
-	private String incomeStatementsQuarterlyApi;
+  @Value("${external-apis.yahoofinance-income-statements-quarterly}")
+  private String incomeStatementsQuarterlyApi;
 
-	@Value("${external-apis.yahoofinance-balance-sheet}")
-	private String balanceSheetApi;
+  @Value("${external-apis.yahoofinance-balance-sheet}")
+  private String balanceSheetApi;
 
-	@Value("${external-apis.yahoofinance-balance-sheet-quarterly}")
-	private String balanceSheetQuarterlyApi;
+  @Value("${external-apis.yahoofinance-balance-sheet-quarterly}")
+  private String balanceSheetQuarterlyApi;
 
-	@Override
-	@SneakyThrows
-	public List<StockCashflowStatement> getCashFlowStatements(String symbol) {
-		String annualRequestUrl = UriComponentsBuilder.fromHttpUrl(cashFlowStatementApi)
-			.buildAndExpand(symbol)
-			.toUriString();
-		String quarterlyRequestUrl = UriComponentsBuilder.fromHttpUrl(cashFlowStatementQuarterlyApi)
-			.buildAndExpand(symbol)
-			.toUriString();
-		ResponseEntity<String> annualResponse = restTemplate.getForEntity(annualRequestUrl, String.class);
-		ResponseEntity<String> quarterlyResponse = restTemplate.getForEntity(quarterlyRequestUrl, String.class);
+  @Override
+  @SneakyThrows
+  public List<StockCashflowStatement> getCashFlowStatements(String symbol) {
+    String annualRequestUrl = UriComponentsBuilder.fromHttpUrl(cashFlowStatementApi)
+            .buildAndExpand(symbol)
+            .toUriString();
+    String quarterlyRequestUrl = UriComponentsBuilder.fromHttpUrl(cashFlowStatementQuarterlyApi)
+            .buildAndExpand(symbol)
+            .toUriString();
+    ResponseEntity<String> annualResponse = restTemplate.getForEntity(annualRequestUrl, String.class);
+    ResponseEntity<String> quarterlyResponse = restTemplate.getForEntity(quarterlyRequestUrl, String.class);
 
-		if ((annualResponse.getStatusCode() == HttpStatus.OK) && quarterlyResponse.getStatusCode() == HttpStatus.OK) {
-			JsonNode annualJson = objectMapper.readTree(annualResponse.getBody());
-			JsonNode quarterlyJson = objectMapper.readTree(quarterlyResponse.getBody());
-			ArrayNode cashFlowStatements = (ArrayNode) annualJson.get("quoteSummary")
-				.get("result")
-				.get(0)
-				.get("cashflowStatementHistory")
-				.get("cashflowStatements");
-			ArrayNode quarterlyCashFlowStatements = (ArrayNode) quarterlyJson.get("quoteSummary")
-				.get("result")
-				.get(0)
-				.get("cashflowStatementHistoryQuarterly")
-				.get("cashflowStatements");
+    if ((annualResponse.getStatusCode() == HttpStatus.OK) && quarterlyResponse.getStatusCode() == HttpStatus.OK) {
+      JsonNode annualJson = objectMapper.readTree(annualResponse.getBody());
+      JsonNode quarterlyJson = objectMapper.readTree(quarterlyResponse.getBody());
+      ArrayNode cashFlowStatements = (ArrayNode) annualJson.get("quoteSummary")
+              .get("result")
+              .get(0)
+              .get("cashflowStatementHistory")
+              .get("cashflowStatements");
+      ArrayNode quarterlyCashFlowStatements = (ArrayNode) quarterlyJson.get("quoteSummary")
+              .get("result")
+              .get(0)
+              .get("cashflowStatementHistoryQuarterly")
+              .get("cashflowStatements");
 
-			Optional<StockCashflowStatement> ttmCashFlowStatements = parseCashflowStatementsFromJson(
-					quarterlyCashFlowStatements)
-				.stream()
-				.reduce(StockCashflowStatement::add);
+      Optional<StockCashflowStatement> ttmCashFlowStatements = parseCashflowStatementsFromJson(
+              quarterlyCashFlowStatements)
+              .stream()
+              .reduce(StockCashflowStatement::add);
 
-			List<StockCashflowStatement> result = parseCashflowStatementsFromJson(cashFlowStatements);
-			ttmCashFlowStatements.ifPresent(stockCashflowStatements -> result.add(0, stockCashflowStatements));
-			return result;
-		}
-		return new ArrayList<>();
-	}
+      List<StockCashflowStatement> result = parseCashflowStatementsFromJson(cashFlowStatements);
+      ttmCashFlowStatements.ifPresent(stockCashflowStatements -> result.add(0, stockCashflowStatements));
+      return result;
+    }
+    return new ArrayList<>();
+  }
 
-	private List<StockCashflowStatement> parseCashflowStatementsFromJson(ArrayNode cashFlowStatements) {
-		List<StockCashflowStatement> stockCashFlowStatementList = new ArrayList<>();
-		for (JsonNode item : cashFlowStatements) {
-			Map<String, Long> jsonWithRawValues = extractRowValueFromEntries(item);
-			StockCashflowStatement translatedObject = objectMapper.convertValue(jsonWithRawValues,
-					StockCashflowStatement.class);
-			translatedObject.setFreeCashFlow(
-					translatedObject.getTotalCashFromOperatingActivities() + translatedObject.getCapitalExpenditures());
-			stockCashFlowStatementList.add(translatedObject);
-		}
-		return stockCashFlowStatementList;
-	}
+  private List<StockCashflowStatement> parseCashflowStatementsFromJson(ArrayNode cashFlowStatements) {
+    List<StockCashflowStatement> stockCashFlowStatementList = new ArrayList<>();
+    for (JsonNode item : cashFlowStatements) {
+      Map<String, Long> jsonWithRawValues = extractRowValueFromEntries(item);
+      StockCashflowStatement translatedObject = objectMapper.convertValue(jsonWithRawValues,
+              StockCashflowStatement.class);
+      translatedObject.setFreeCashFlow(
+              translatedObject.getTotalCashFromOperatingActivities() + translatedObject.getCapitalExpenditures());
+      stockCashFlowStatementList.add(translatedObject);
+    }
+    return stockCashFlowStatementList;
+  }
 
-	private Map<String, Long> extractRowValueFromEntries(JsonNode item) {
-		Map<String, Long> jsonWithRawValues = new HashMap<>();
-		item.fields().forEachRemaining(entry -> {
-			JsonNode value = entry.getValue();
-			if (value.has("raw")) {
-				if (entry.getKey().equals("endDate")) {
-					jsonWithRawValues.put(entry.getKey(), value.get("raw").asLong() * 1000);
-					return;
-				}
-				jsonWithRawValues.put(entry.getKey(), value.get("raw").asLong());
-			}
-		});
-		return jsonWithRawValues;
-	}
+  private Map<String, Long> extractRowValueFromEntries(JsonNode item) {
+    Map<String, Long> jsonWithRawValues = new HashMap<>();
+    item.fields().forEachRemaining(entry -> {
+      JsonNode value = entry.getValue();
+      if (value.has("raw")) {
+        if (entry.getKey().equals("endDate")) {
+          jsonWithRawValues.put(entry.getKey(), value.get("raw").asLong() * 1000);
+          return;
+        }
+        jsonWithRawValues.put(entry.getKey(), value.get("raw").asLong());
+      }
+    });
+    return jsonWithRawValues;
+  }
 
-	@Override
-	@SneakyThrows
-	public List<StockIncomeStatement> getIncomeStatements(String symbol) {
-		String annualRequestUrl = UriComponentsBuilder.fromHttpUrl(incomeStatementsApi)
-			.buildAndExpand(symbol)
-			.toUriString();
-		String quarterlyRequestUrl = UriComponentsBuilder.fromHttpUrl(incomeStatementsQuarterlyApi)
-			.buildAndExpand(symbol)
-			.toUriString();
+  @Override
+  @SneakyThrows
+  public List<StockIncomeStatement> getIncomeStatements(String symbol) {
+    String annualRequestUrl = UriComponentsBuilder.fromHttpUrl(incomeStatementsApi)
+            .buildAndExpand(symbol)
+            .toUriString();
+    String quarterlyRequestUrl = UriComponentsBuilder.fromHttpUrl(incomeStatementsQuarterlyApi)
+            .buildAndExpand(symbol)
+            .toUriString();
 
-		ResponseEntity<String> annualResponse = restTemplate.getForEntity(annualRequestUrl, String.class);
-		ResponseEntity<String> quarterlyResponse = restTemplate.getForEntity(quarterlyRequestUrl, String.class);
-		if ((annualResponse.getStatusCode() == HttpStatus.OK) && quarterlyResponse.getStatusCode() == HttpStatus.OK) {
-			JsonNode annualJson = objectMapper.readTree(annualResponse.getBody());
-			JsonNode quarterlyJson = objectMapper.readTree(quarterlyResponse.getBody());
-			ArrayNode incomeStatements = (ArrayNode) annualJson.get("quoteSummary")
-				.get("result")
-				.get(0)
-				.get("incomeStatementHistory")
-				.get("incomeStatementHistory");
-			ArrayNode quarterlyIncomeStatements = (ArrayNode) quarterlyJson.get("quoteSummary")
-				.get("result")
-				.get(0)
-				.get("incomeStatementHistoryQuarterly")
-				.get("incomeStatementHistory");
+    ResponseEntity<String> annualResponse = restTemplate.getForEntity(annualRequestUrl, String.class);
+    ResponseEntity<String> quarterlyResponse = restTemplate.getForEntity(quarterlyRequestUrl, String.class);
+    if ((annualResponse.getStatusCode() == HttpStatus.OK) && quarterlyResponse.getStatusCode() == HttpStatus.OK) {
+      JsonNode annualJson = objectMapper.readTree(annualResponse.getBody());
+      JsonNode quarterlyJson = objectMapper.readTree(quarterlyResponse.getBody());
+      ArrayNode incomeStatements = (ArrayNode) annualJson.get("quoteSummary")
+              .get("result")
+              .get(0)
+              .get("incomeStatementHistory")
+              .get("incomeStatementHistory");
+      ArrayNode quarterlyIncomeStatements = (ArrayNode) quarterlyJson.get("quoteSummary")
+              .get("result")
+              .get(0)
+              .get("incomeStatementHistoryQuarterly")
+              .get("incomeStatementHistory");
 
-			Optional<StockIncomeStatement> ttmIncomeStatements = parseStockIncomeStatementsFromJson(
-					quarterlyIncomeStatements)
-				.stream()
-				.reduce(StockIncomeStatement::add);
+      Optional<StockIncomeStatement> ttmIncomeStatements = parseStockIncomeStatementsFromJson(
+              quarterlyIncomeStatements)
+              .stream()
+              .reduce(StockIncomeStatement::add);
 
-			List<StockIncomeStatement> result = parseStockIncomeStatementsFromJson(incomeStatements);
-			ttmIncomeStatements.ifPresent(stockIncomeStatement -> result.add(0, stockIncomeStatement));
-			return result;
-		}
-		return new ArrayList<>();
-	}
+      List<StockIncomeStatement> result = parseStockIncomeStatementsFromJson(incomeStatements);
+      ttmIncomeStatements.ifPresent(stockIncomeStatement -> result.add(0, stockIncomeStatement));
+      return result;
+    }
+    return new ArrayList<>();
+  }
 
-	@Override
-	@SneakyThrows
-	public List<StockBalanceSheet> getBalanceSheets(String symbol) {
-		String annualRequestUrl = UriComponentsBuilder.fromHttpUrl(balanceSheetApi)
-			.buildAndExpand(symbol)
-			.toUriString();
-		String quarterlyRequestUrl = UriComponentsBuilder.fromHttpUrl(balanceSheetQuarterlyApi)
-			.buildAndExpand(symbol)
-			.toUriString();
-		ResponseEntity<String> annualResponse = restTemplate.getForEntity(annualRequestUrl, String.class);
-		ResponseEntity<String> quarterlyResponse = restTemplate.getForEntity(quarterlyRequestUrl, String.class);
-		if ((annualResponse.getStatusCode() == HttpStatus.OK) && quarterlyResponse.getStatusCode() == HttpStatus.OK) {
-			JsonNode annualJson = objectMapper.readTree(annualResponse.getBody());
-			JsonNode quarterlyJson = objectMapper.readTree(quarterlyResponse.getBody());
-			ArrayNode balanceSheets = (ArrayNode) annualJson.get("quoteSummary")
-				.get("result")
-				.get(0)
-				.get("balanceSheetHistory")
-				.get("balanceSheetStatements");
-			ArrayNode quarterlyBalanceSheets = (ArrayNode) quarterlyJson.get("quoteSummary")
-				.get("result")
-				.get(0)
-				.get("balanceSheetHistoryQuarterly")
-				.get("balanceSheetStatements");
-			List<StockBalanceSheet> stockBalanceSheetsList = new ArrayList<>();
+  @Override
+  @SneakyThrows
+  public List<StockBalanceSheet> getBalanceSheets(String symbol) {
+    String annualRequestUrl = UriComponentsBuilder.fromHttpUrl(balanceSheetApi)
+            .buildAndExpand(symbol)
+            .toUriString();
+    String quarterlyRequestUrl = UriComponentsBuilder.fromHttpUrl(balanceSheetQuarterlyApi)
+            .buildAndExpand(symbol)
+            .toUriString();
+    ResponseEntity<String> annualResponse = restTemplate.getForEntity(annualRequestUrl, String.class);
+    ResponseEntity<String> quarterlyResponse = restTemplate.getForEntity(quarterlyRequestUrl, String.class);
+    if ((annualResponse.getStatusCode() == HttpStatus.OK) && quarterlyResponse.getStatusCode() == HttpStatus.OK) {
+      JsonNode annualJson = objectMapper.readTree(annualResponse.getBody());
+      JsonNode quarterlyJson = objectMapper.readTree(quarterlyResponse.getBody());
+      ArrayNode balanceSheets = (ArrayNode) annualJson.get("quoteSummary")
+              .get("result")
+              .get(0)
+              .get("balanceSheetHistory")
+              .get("balanceSheetStatements");
+      ArrayNode quarterlyBalanceSheets = (ArrayNode) quarterlyJson.get("quoteSummary")
+              .get("result")
+              .get(0)
+              .get("balanceSheetHistoryQuarterly")
+              .get("balanceSheetStatements");
+      List<StockBalanceSheet> stockBalanceSheetsList = new ArrayList<>();
 
-			StockBalanceSheet ttmBalanceSheet = null;
+      StockBalanceSheet ttmBalanceSheet = null;
 
-			for (JsonNode item : quarterlyBalanceSheets) {
-				StockBalanceSheet convertedValue = objectMapper.convertValue(item, StockBalanceSheet.class);
-				if (ttmBalanceSheet == null) {
-					ttmBalanceSheet = convertedValue;
-				}
-				else {
-					ttmBalanceSheet.add(convertedValue);
-				}
-			}
+      for (JsonNode item : quarterlyBalanceSheets) {
+        StockBalanceSheet convertedValue = objectMapper.convertValue(item, StockBalanceSheet.class);
+        if (ttmBalanceSheet == null) {
+          ttmBalanceSheet = convertedValue;
+        } else {
+          ttmBalanceSheet.add(convertedValue);
+        }
+      }
 
-			for (JsonNode item : balanceSheets) {
-				stockBalanceSheetsList.add(objectMapper.convertValue(item, StockBalanceSheet.class));
-			}
+      for (JsonNode item : balanceSheets) {
+        stockBalanceSheetsList.add(objectMapper.convertValue(item, StockBalanceSheet.class));
+      }
 
-			stockBalanceSheetsList.add(0, ttmBalanceSheet);
+      stockBalanceSheetsList.add(0, ttmBalanceSheet);
 
-			return stockBalanceSheetsList;
-		}
-		return new ArrayList<>();
-	}
+      return stockBalanceSheetsList;
+    }
+    return new ArrayList<>();
+  }
 
-	private List<StockIncomeStatement> parseStockIncomeStatementsFromJson(ArrayNode stockIncomeStatementJson) {
-		List<StockIncomeStatement> stockIncomeStatementList = new ArrayList<>();
-		for (JsonNode item : stockIncomeStatementJson) {
-			Map<String, Long> jsonWithRawValues = extractRowValueFromEntries(item);
-			StockIncomeStatement translatedObject = objectMapper.convertValue(jsonWithRawValues,
-					StockIncomeStatement.class);
-			stockIncomeStatementList.add(translatedObject);
-		}
-		return stockIncomeStatementList;
-	}
+  private List<StockIncomeStatement> parseStockIncomeStatementsFromJson(ArrayNode stockIncomeStatementJson) {
+    List<StockIncomeStatement> stockIncomeStatementList = new ArrayList<>();
+    for (JsonNode item : stockIncomeStatementJson) {
+      Map<String, Long> jsonWithRawValues = extractRowValueFromEntries(item);
+      StockIncomeStatement translatedObject = objectMapper.convertValue(jsonWithRawValues,
+              StockIncomeStatement.class);
+      stockIncomeStatementList.add(translatedObject);
+    }
+    return stockIncomeStatementList;
+  }
 
 }
