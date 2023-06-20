dfc0815f8dc42e3d4a47e458189739f33211d567
Nagy Adam <raween2000@gmail.com>
2023-06-20 21:02:40 +0200

    UserStockHistoryService test

--- src/main/java/com/adamnagyan/yahoofinancewebapi/user_stock/mapper/StockHistoryMapper.java
+++ src/main/java/com/adamnagyan/yahoofinancewebapi/user_stock/mapper/StockHistoryMapper.java
@@ -1,16 +1,20 @@
 package com.adamnagyan.yahoofinancewebapi.user_stock.mapper;
 
+import com.adamnagyan.yahoofinancewebapi.auth.mapper.AuthenticationRequestMapper;
 import com.adamnagyan.yahoofinancewebapi.user_stock.dto.StockHistoryItemDto;
 import com.adamnagyan.yahoofinancewebapi.user_stock.dto.StockHistoryListDto;
 import com.adamnagyan.yahoofinancewebapi.user_stock.dto.UserStockHistoryItemRequestDto;
 import com.adamnagyan.yahoofinancewebapi.user_stock.model.StockHistoryItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper
 public interface StockHistoryMapper {
+
+	StockHistoryMapper INSTANCE = Mappers.getMapper(StockHistoryMapper.class);
 
 	@Mapping(target = "fullPrice",
 			expression = "java(stockHistoryItem.getPrice() * stockHistoryItem.getQuantity() + stockHistoryItem.getTransactionFee())")
--- src/test/java/com/adamnagyan/yahoofinancewebapi/user_stock/service/UserStockHistoryServiceImplTest.java
+++ src/test/java/com/adamnagyan/yahoofinancewebapi/user_stock/service/UserStockHistoryServiceImplTest.java
@@ -2,17 +2,21 @@ package com.adamnagyan.yahoofinancewebapi.user_stock.service;
 
 
 import com.adamnagyan.yahoofinancewebapi.auth.model.User;
+import com.adamnagyan.yahoofinancewebapi.common.exceptions.BaseAppException;
+import com.adamnagyan.yahoofinancewebapi.user_stock.dto.StockHistoryListDto;
 import com.adamnagyan.yahoofinancewebapi.user_stock.dto.UserStockHistoryItemRequestDto;
 import com.adamnagyan.yahoofinancewebapi.user_stock.mapper.StockHistoryMapper;
 import com.adamnagyan.yahoofinancewebapi.user_stock.model.StockHistoryItem;
 import com.adamnagyan.yahoofinancewebapi.user_stock.respository.UserStockRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class UserStockHistoryServiceImplTest {
 
@@ -21,7 +25,6 @@ public class UserStockHistoryServiceImplTest {
   @Mock
   UserStockRepository userStockRepository;
 
-  @Mock
   StockHistoryMapper stockHistoryMapper;
 
   User user = User.builder().id(1L).email("test@example.com").build();
@@ -48,30 +51,72 @@ public class UserStockHistoryServiceImplTest {
   @Before
   public void setUp() {
     MockitoAnnotations.openMocks(this);
+    stockHistoryMapper = StockHistoryMapper.INSTANCE;
     userStockHistoryServiceImpl = new UserStockHistoryServiceImpl(userStockRepository, stockHistoryMapper);
   }
 
   @Test
-  public void addNewUserStock() {
-    userStockHistoryServiceImpl.addNewUserStock(request, user);
+  public void updateStockById() {
+    StockHistoryItem stockHistoryItem = StockHistoryItem.builder()
+            .symbol("AAPL")
+            .quantity(10.0)
+            .transactionFee(1.0)
+            .price(100.0)
+            .user(user)
+            .buyDate(DATE)
+            .build();
+    Mockito.when(userStockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(stockHistoryItem));
+    userStockHistoryServiceImpl.updateStockById(1L, request, user);
+    stockHistoryItem.setUser(user);
+    stockHistoryItem.setId(1L);
+    Mockito.verify(userStockRepository).save(stockHistoryItem);
+  }
+
+  @Test(expected = BaseAppException.class)
+  public void updateStockByIdWithWrongUser() {
+    StockHistoryItem stockHistoryItem = StockHistoryItem.builder()
+            .symbol("AAPL")
+            .quantity(10.0)
+            .transactionFee(1.0)
+            .price(100.0)
+            .user(user)
+            .buyDate(DATE)
+            .build();
+    Mockito.when(userStockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(stockHistoryItem));
+    userStockHistoryServiceImpl.updateStockById(1L, request,
+            User.builder()
+            .email("test2@example.com")
+            .id(2L).build()
+    );
+    stockHistoryItem.setUser(user);
+    stockHistoryItem.setId(1L);
     Mockito.verify(userStockRepository).save(stockHistoryItem);
   }
 
   @Test
-  public void updateStockById() {
-
-  }
-
-  @Test
   public void getAllUserStockHistoryItem() {
+    Mockito.when(userStockRepository.findAllByUser(Mockito.any(User.class))).thenReturn(java.util.List.of(stockHistoryItem));
+    StockHistoryListDto stockHistoryListDto = userStockHistoryServiceImpl.getAllUserStockHistoryItem(user);
+    Mockito.verify(userStockRepository).findAllByUser(Mockito.any(User.class));
+    Assertions.assertEquals(stockHistoryListDto.getStockHistoryItems().size(), 1);
   }
 
   @Test
   public void deleteUserStockById() {
+    Mockito.when(userStockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(stockHistoryItem));
+    userStockHistoryServiceImpl.deleteUserStockById(1L, user);
+    Mockito.verify(userStockRepository).deleteById(Mockito.anyLong());
   }
 
-  @Test
-  public void isItemRelatedToUser() {
+  @Test(expected = BaseAppException.class)
+  public void deleteUserStockByIdWithWrongUser() {
+    Mockito.when(userStockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(stockHistoryItem));
+    userStockHistoryServiceImpl.deleteUserStockById(1L,
+            User.builder()
+                    .email("test2@example.com")
+                    .id(2L).build()
+    );
   }
+
 
 }
